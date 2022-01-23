2c4a76522b21393b0f5e7021225f6cebf43b92c7
KamPatel1975 <97128198+KamPatel1975@users.noreply.github.com>
2022-01-23 13:38:29 +0000

    Update DoWhiletables8.java

--- DoWhiletables8.java
+++ DoWhiletables8.java
@@ -4,7 +4,7 @@ public class DoWhiletables8 {
     public static void main (String[]args){
         int a=1;
         do {
-            System.out.println(a*8);
+            System.out.println(8 + " * " +a+"="+ (a*8));
             a++;
 
         }while (a<=10);
