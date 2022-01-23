aba36c20c426055735a4690f72a47faa0118ec14
KamPatel1975 <97128198+KamPatel1975@users.noreply.github.com>
2022-01-23 13:40:28 +0000

    Update WhileLoopTable9.java

--- WhileLoopTable9.java
+++ WhileLoopTable9.java
@@ -4,7 +4,7 @@ public class WhileLoopTable9 {
     public static void main (String[]args){
         int i=1;
         while(i<10){
-            System.out.println(i*9);
+            System.out.println(9 + " * " + i + " = " + (9 * i));
             i++;
         }
     }
