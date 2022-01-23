4145dbf5073f5b1f11b916279c346f3d0db96a8a
KamPatel1975 <97128198+KamPatel1975@users.noreply.github.com>
2022-01-23 13:39:47 +0000

    Update ForLoopTableOf10.java

--- ForLoopTableOf10.java
+++ ForLoopTableOf10.java
@@ -3,6 +3,6 @@ package Java3Homework;
 public class ForLoopTableOf10 {
     public static void main (String [] args){
         for (int i = 0; i<=101; i++)
-            System.out.println(i*10);
+            System.out.println(10+"*" +i+ "=" +(i*10));
 }
 }
